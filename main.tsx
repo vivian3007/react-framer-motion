badacdb0c079b5053ad891975f2e14434e27ddb4
vivian <1022620@hr.nl>
2025-09-15 15:05:55 +0200

    title

--- main.tsx
+++ main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <h1>Hellooooooo</h1>
+        <h1>Drag and drop!</h1>
         <App />
     </React.StrictMode>,
 )
